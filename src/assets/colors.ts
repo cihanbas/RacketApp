692bdc7d9ed624bda8709ac868094d25bea1c3a6
cihanbas <cihanbas07@gmail.com>
2022-10-01 15:07:00 +0300

    added new colors

--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -2,8 +2,13 @@ const colors = {
     yellow: "#FFC303",
     lightYellow: "#FFC34E",
     paleYellow: 'rgba(255, 195, 3,0.2)',
-    textColor: "#000",
-    blue:"#1FB0F7"
+    black: "#000",
+    blue: "#1FB0F7",
+    white: '#fff',
+    modalBackground: 'rgba(0,0,0,.5)',
+    gradientStartColor:'#0180ae',
+    gradientEndColor:'#23b99b',
+
 
 }
 export { colors }
