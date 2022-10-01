e3edcfa72614364dd389429f5e2a91daa3824998
cihanbas <cihanbas07@gmail.com>
2022-10-01 12:25:25 +0300

    created the navigation type

---
+++ src/navigation/type.ts
@@ -0,0 +1,21 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+export type StackNavigationType = {
+  Home: undefined;
+  RateUs: undefined;
+};
+
+export type SubNavigator<T extends StackNavigationType> = {
+  [K in keyof T]: { screen: K; params?: T[K] };
+}[keyof T];
+export type NavigationStackProps = NativeStackScreenProps<
+  StackNavigationType
+>;
+export type DefaultNavigationProps<T extends keyof StackNavigationType> =
+  NativeStackScreenProps<StackNavigationType, T>;
+export type NavigationProps = {
+  navigation: NavigationStackProps['navigation'];
+};
+export type NavigationRouteProps = {
+  route: NavigationStackProps['route'];
+};
