502a521daa3b4da19d989d71c3d68aac52a6b3ba
cihanbas <cihanbas07@gmail.com>
2022-10-05 18:06:20 +0300

    rateApp helper completed

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,7 +1,24 @@
+import { Linking, Platform } from 'react-native';
 import { moderateScale } from 'react-native-size-matters';
-
+const isIOS = Platform.OS === 'ios'
 function normalize(number: number, factor = 0.25) {
     return moderateScale(number, factor);
 }
 const appPading = normalize(24)
-export { normalize,appPading };
+
+const link ='itms-apps://apps.apple.com/tr/app/times-tables-lets-learn/id1055437768?l=tr';
+
+ 
+export const rateApp = () => {
+    if (isIOS) {
+        Linking.canOpenURL(link).then(supported => { 
+            supported && Linking.openURL(link);
+          }, (err) => alert('Something went wrong'));
+    }
+    else {
+        Linking.openURL("https://play.google.com/store/apps/details?id=com.racketpal")
+
+    }
+
+}
+export { normalize, appPading };
